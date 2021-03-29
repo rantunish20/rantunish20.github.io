ba57467f5c5b5f07da1f5249341ac1f64ab73161
Romel <rantunish20@gmail.com>
2021-03-29 09:35:30 -0400

    Complete with JS

---
+++ script.js
@@ -0,0 +1,7 @@
+//Button to scroll scroll to top
+var mybutton = document.getElementById(".scrollToTopBtn");
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
